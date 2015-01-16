efa856cc72087acbd83d2115cb2b19294b36419d
philipjrosen <philipjrosen@gmail.com>
2015-01-16 13:10:59 -0800

    implements LinksController

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,16 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $http, Links) {
+  $scope.data = {};
+  $scope.getLinks = function(links) {
+    $scope.data.links = links;
+  };
+
+  $http.get('/api/links').
+    success(function(links, status, headers, config) {
+      $scope.getLinks(links);
+    }).
+    error(function(data, status, headers, config) {
+      console.log(error);
+    });
 });
