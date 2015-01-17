4a71f6ea3f548248019b64d3833bac5bbae23920
philipjrosen <philipjrosen@gmail.com>
2015-01-16 21:16:58 -0800

    adds data.links property explicitly as array

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,11 +2,11 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, $http, Links) {
   $scope.data = {};
+  $scope.data.links = [];
 
-  $scope.getLinks = function () {
+  $scope.getLinks = function() {
     Links.getLinks()
       .then(function (data) {
-        console.log('links', data.links);
         $scope.data.links = data.data;
       })
       .catch(function (error) {
