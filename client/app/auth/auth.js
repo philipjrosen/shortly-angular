b276972c0b5edd99413b2596cfe3fb97b4424328
philipjrosen <philipjrosen@gmail.com>
2015-01-16 21:18:06 -0800

    redirects to /shorten on signup

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -21,7 +21,7 @@ angular.module('shortly.auth', [])
     Auth.signup($scope.user)
       .then(function (token) {
         $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
+        $location.path('/shorten');
       })
       .catch(function (error) {
         console.error(error);
